a63c2f042a5b28a4b41ffdd10b3bd69ffa9b24e8
Taqiy <taqiyjayya@gmail.com>
2025-11-18 21:29:08 +0700

    Comment out login and register API routes
    Comment out login and register routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+//Route::post('/login', [AuthController::class, 'login']);
+//Route::post('/register', [AuthController::class, 'register']);
 Route::post('password/email', [PasswordResetController::class, 'sendResetLinkEmail']);
 Route::post('password/reset', [PasswordResetController::class, 'resetPassword']);
 
